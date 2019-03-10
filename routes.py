cb0258b03273cf45db6e305f60a361d725c548d1
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-03-10 21:57:50 +0530

    create database to contain form data

--- routes.py
+++ routes.py
@@ -1,6 +1,32 @@
 from flask import Flask, render_template, request
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    taskname = db.Column(db.String(80), unique=True)
+    completedate = db.Column(db.DateTime())
+    category = db.Column(db.String(20))
+
+    def __init__(self, taskname, completedate, category):
+        self.taskname = taskname
+        self.completedate = completedate
+        self.category = category
+
+class TaskSchema(ma.Schema):
+    class Meta:
+        fields = ('taskname', 'completedate','category')
+
+task_schema = TaskSchema()
+tasks_schema = TaskSchema(many=True)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
@@ -8,6 +34,7 @@ def form():
 
 @app.route('/form_update', methods=['GET', 'POST'])
 def form_update():
+
     if request.form['taskcategory'] == "To Do":
         return render_template('kanban.html', update_todo=request.form['taskname'])
     elif request.form['taskcategory'] == "Doing":
